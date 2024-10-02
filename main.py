e2118951552ba6d801360ca1688bd16501b10f88
Alemilsonn Fabiano Silva <alemilson@ice.ufjf.br>
2024-10-02 12:31:03 -0300

    Ajuste de parâmetros

--- main.py
+++ main.py
@@ -16,13 +16,12 @@ def read_parameters(file_path):
         for line in file:
             line = line.strip()
             if line.startswith('#') or not line:
-                continue  # Ignorar comentários e linhas em branco
+                continue
 
             parts = line.split()
             param_name = parts[0]
             param_value = float(parts[-1])
 
-            # Identificando o tipo de parâmetro
             if param_name.startswith('custo_'):
                 if param_name.split('_')[1] in ['boi', 'porco', 'frango']:
                     animal = param_name.split('_')[1]
@@ -41,18 +40,15 @@ def read_parameters(file_path):
                     piece = '_'.join(param_name.split('_')[1:])
                     S_p[piece] = param_value
             elif param_name.startswith('capacidade_total_animais'):
-                # Captura da capacidade total de armazenamento para animais
                 S_total_animais = param_value
             elif param_name.startswith('capacidade_total_pecas'):
-                # Captura da capacidade total de armazenamento para peças
                 S_total_pecas = param_value
             else:
-                # Lidando com peças que possuem mais de um underscore
                 try:
                     animal, piece = param_name.split('_', 1)
                     q[(animal, piece)] = param_value
                 except ValueError:
-                    pass  # Ignora linhas mal formatadas
+                    pass
 
     return q, C_W, C_P, D, S_a, S_p, S_total_animais, S_total_pecas
 
@@ -60,19 +56,16 @@ def read_parameters(file_path):
 # Criando o modelo
 model = ConcreteModel()
 
-# Definindo conjuntos
 model.Animais = Set(initialize=['boi', 'porco', 'frango'])
 model.Pecas = Set(initialize=[
     'file_mignon', 'picanha', 'alcatra', 'contra_file', 'cha_dentro', 'cha_fora',
     'lagarto', 'patinho', 'acem', 'musculo', 'paleta', 'peito', 'costela_s_osso',
     'costela_c_osso', 'figado', 'costela_suina', 'lombo', 'pernil', 'bacon',
-    'linguica', 'peito_frango', 'coxa_sobrecoxa', 'asa', 'file_frango'
+    'linguica', 'peito_frango', 'coxa_sobrecoxa', 'asa', 'moela_frango'
 ])
 
-# Lendo parâmetros do arquivo
 q, C_W, C_P, D, S_a, S_p, S_total_animais, S_total_pecas = read_parameters('parametros.txt')
 
-# Definindo parâmetros
 model.q = Param(model.Animais, model.Pecas, initialize=q, default=0)
 model.C_W = Param(model.Animais, initialize=C_W, default=0)
 model.C_P = Param(model.Pecas, initialize=C_P, default=0)
@@ -82,7 +75,6 @@ model.S_p = Param(model.Pecas, initialize=S_p, default=0)
 model.S_total_animais = Param(initialize=S_total_animais)
 model.S_total_pecas = Param(initialize=S_total_pecas)
 
-# Definindo variáveis de decisão
 model.W = Var(model.Animais, within=NonNegativeIntegers)
 model.P = Var(model.Pecas, within=NonNegativeIntegers)
 
@@ -98,10 +90,8 @@ model.obj = Objective(rule=objective_function, sense=minimize)
 
 # Restrições de demanda
 def demand_constraints(model, p):
-    return (sum(model.q[a, p] * model.W[a]  for a in model.Animais) +
+    return (sum(model.q[a, p] * model.W[a] for a in model.Animais) +
             model.P[p]) >= model.D[p]
-
-model.demand = Constraint(model.Pecas, rule=demand_constraints)
 
 
 model.demand = Constraint(model.Pecas, rule=demand_constraints)
@@ -122,16 +112,17 @@ def storage_constraint_pecas(model):
 
 model.storage_pecas = Constraint(rule=storage_constraint_pecas)
 
-# Resolvendo o modelo
 solver = SolverFactory('glpk')
 results = solver.solve(model, tee=True)
 
-# Exibindo os resultados
 print("\nResultados da Otimização:")
 for a in model.Animais:
     print(f"Número de {a}s comprados: {model.W[a].value}")
 
 for p in model.Pecas:
-    print(f"Quantidade de {p}s comprados: {model.P[p].value} kg")
+    if (p != 'frango_assado'):
+        print(f"Quantidade de {p}s comprados: {model.P[p].value} kg")
+
 
 print("\nCusto total: R$", model.obj())
+
